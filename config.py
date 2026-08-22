187ae587e5850b51eac242c5be1c1578cc392deb
Abram <git.skimming549@passinbox.com>
2026-08-22 01:49:56 +0100

    refactor: add config for dev stack and secret IP retrieval

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 from dataclasses import dataclass
 
+import pulumi
+
 
 @dataclass
 class EnvConfig:
     env_name: str
+    local_ip: pulumi.Output[str]
 
 
 def load_env_config() -> EnvConfig:
-    import pulumi
-
+    config = pulumi.Config()
     stack = pulumi.get_stack()
     env_config = EnvConfig(
         env_name=stack,
+        local_ip=config.require_secret("local_ip"),
     )
     return env_config
