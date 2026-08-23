d80e8fd44171b060a3cf0b1581471702ee032714
Abram <git.skimming549@passinbox.com>
2026-08-23 12:55:20 +0100

    refactor: add RDS master password config support

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ import pulumi
 class EnvConfig:
     env_name: str
     local_ip: pulumi.Output[str]
+    rds_master_password: pulumi.Output[str]
 
 
 def load_env_config() -> EnvConfig:
@@ -15,5 +16,6 @@ def load_env_config() -> EnvConfig:
     env_config = EnvConfig(
         env_name=stack,
         local_ip=config.require_secret("local_ip"),
+        rds_master_password=config.require_secret("rds_master_password"),
     )
     return env_config
